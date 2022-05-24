ccf8abb701a996af264a9e4b84962755a314dc67
Lailson Sobral <lailsonlm@hotmail.com>
2022-05-24 20:41:50 -0300

    firebase databaseURL

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,6 +5,7 @@ import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SEND_ID,
